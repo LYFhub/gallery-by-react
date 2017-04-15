7124c4300534d841cd32c9d69747cc850517aa21
LYFhub <liyf963@163.com>
2017-04-15 22:02:07 +0800

    '更新舞台组件，设置每张图片的位置'

--- src/components/Main.js
+++ src/components/Main.js
@@ -25,30 +25,119 @@ class ImgFigure extends React.Component {
 	render() {
 		return (
 			<figure className="img-figure">
-				<img src={this.props.data.imageURL} alt={this.props.data.title}></img> 
+				<img src={this.props.data.imageURL} alt={this.props.data.title}></img>
 				<figcaption>
 					<h2 className="img-title">{this.props.data.title}</h2>
 				</figcaption>
 			</figure>
 		);
 	}
-};
+}
 
 // 图片画廊组件
 // 循环遍历图片库，将生成的子组件压入数组，便于插入父组件中
 class GalleryByReactApp extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			imgsArrangeArr: [
+				/*
+					{
+						pso: {
+							left: '0',
+							top: '0'
+						}
+					}
+					*/
+			]
+		};
+	}
+
+	Constant: {
+		centerPos: {
+			left: 0,
+			right: 0
+		},
+		hPosRange: {
+			leftSecX: [0, 0],
+			rightSecX: [0, 0],
+			y: [0, 0]
+		},
+		vPosRange: {
+			x: [0, 0],
+			topY: [0, 0]
+		}
+	}
+
+	/*
+	 * 重新布局所有图片
+	 * @param centerIndex 指定居中排布哪个图片
+	 */
+	rearrange(centerIndex) {
+
+	}
+
+	//组件加载之后，为每张图片计算其未知的范围
+	componentDidMount() {
+		// 首先拿到舞台的大小
+		var stageDOM = this.refs.stage,
+			stageW = stageDOM.scrollWidth,
+			stageH = stageDOM.scrollHeight,
+			halfStageW = Math.ceil(stageW / 2),
+			halfStageH = Math.ceil(stageH / 2);
+
+		// 拿到一个imageFigure 的大小
+		var imgFigureDOM = this.refs.imgFigure0,
+			imgW = imgFigureDOM.scrollWidth,
+			imgH = imgFigureDOM.scrollHeight,
+			halfImgW = Math.ceil(imgW / 2),
+			halfImgH = Math.ceil(imgH / 2);
+
+		// 计算中心图片的位置点
+		this.Constant.centerPos = {
+			left: halfStageW - halfImgW,
+			top: halfStageH - halfImgH
+		}
+
+
+		// 计算左侧右侧区域图片排布位置的取值范围
+		this.Constant.hPosRange.leftSecX[0] = -halfImgW;
+		this.Constant.hPosRange.leftSecX[1] = halfStageW - halfImgW * 3;
+		this.Constant.hPosRange.rightSecX[0] = halfStageW - halfImgW;
+		this.Constant.hPosRange.rightSecX[1] = stageW - halfImgW;
+		this.Constant.hPosRange.y[0] = -halfImgH;
+		this.Constant.hPosRange.y[1] = stageH = halfImgH;
+
+		// 计算上侧区域图片排布位置的取值范围
+		this.Constant.vPosRange.topY[0] = -halfImgH;
+		this.Constant.vPosRange.topY[1] = halfStageH - halfImgH * 3;
+		this.Constant.vPosRange.x[0] = halfImgW - imgW;
+		this.Constant.vPosRange.x[1] = halfImgW;
+
+		this.rearrange(0);
+	}
 
 	render() {
 
 		var controllerUnits = [],
 			imgFigures = [];
 
-		imageDatas.forEach(function(value) {
-			imgFigures.push(<ImgFigure data={value}/>);
-		});
+		imageDatas.forEach(function(value, index) {
+
+			if (!this.state.imgsArrangeArr[index]) {
+				this.state.imgsArrangeArr[index] = {
+					pos: {
+						left: 0,
+						top: 0
+					}
+				}
+			}
+
+			imgFigures.push(<ImgFigure data={value} ref={'imgFigure' + index} key={'imgFigure' + index}/>);
+		}.bind(this));
 
 		return (
-			<section className="stage">
+			<section className="stage" ref="stage">
 				<section className="img-sec">
 					{imgFigures}
 				</section>
@@ -58,7 +147,7 @@ class GalleryByReactApp extends React.Component {
 			</section>
 		);
 	}
-};
+}
 
 GalleryByReactApp.defaultProps = {};
 
